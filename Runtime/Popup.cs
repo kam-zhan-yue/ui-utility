2396ff1ff127990bd9b39a1a6fa82aa9e47bea83
kamzhanyue <kamzhanyue@gmail.com>
2023-10-15 02:14:57 +0800

    Add Odin Stuff

--- Runtime/Popup.cs
+++ Runtime/Popup.cs
@@ -1,21 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Kuroneko.UIUtility
 {
-    public class Popup : MonoBehaviour
+    public abstract class Popup : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [BoxGroup("UI Objects")]
+        public RectTransform mainHolder;
+    
+        [NonSerialized, ShowInInspector, ReadOnly]
+        public bool isAnimating = false;
+    
+        [NonSerialized, ShowInInspector, ReadOnly]
+        public bool isShowing = false;
+    
+        public Action<Popup> onCloseButtonClicked = null;
+
+        private void Awake()
         {
-            
+            InitPopup();
         }
 
-        // Update is called once per frame
-        void Update()
+        protected abstract void InitPopup();
+
+        public virtual void ShowPopup()
         {
-            
+            isShowing = true;
+            mainHolder.gameObject.SetActive(true);
+        }
+
+        public virtual void HidePopup()
+        {
+            isShowing = false;
+            mainHolder.gameObject.SetActive(false);
+        }
+    
+        public virtual void CloseButtonClicked()
+        {
+            onCloseButtonClicked?.Invoke(this);
+        }
+
+        public virtual void EscapeButtonClicked()
+        {
+            CloseButtonClicked();
         }
     }
 }
